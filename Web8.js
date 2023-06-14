33398bb46d9ed2efd27d91738decedd4fbc85145
AKoornneefFleming <adrian.koornneef@flemingcollege.ca>
2023-06-14 17:08:44 -0400

    AK_PhotoGallery1

--- Web8.js
+++ Web8.js
@@ -1,4 +1,4 @@
-// Javascript from w3 schools
+// Original Javascript from w3 schools, minor modification
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_navbar_slide
 // When the user scrolls down a designated distance from the top of the document, slide down the navbar 
 
@@ -11,3 +11,35 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-100px";
   }
 }
+
+    /***********************Photo Gallery**********************/
+// Original Jasvascript from w3 schools, only minor modification to referenced classes
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow 
+//  Consulted Bing Chat for information on 'onload' events to debug migration from sample in HTML -AK
+
+let slideIndex = 1;
+            showSlides(slideIndex);
+            
+            function plusSlides(n) {
+              showSlides(slideIndex += n);
+            }
+            
+            function currentSlide(n) {
+              showSlides(slideIndex = n);
+            }
+            
+            function showSlides(n) {
+              let i;
+              let slides = document.getElementsByClassName("mySlides");
+              let dots = document.getElementsByClassName("gallery_dot");
+              if (n > slides.length) {slideIndex = 1}    
+              if (n < 1) {slideIndex = slides.length}
+              for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
+              }
+              for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" gallery_active", "");
+              }
+              slides[slideIndex-1].style.display = "block";  
+              dots[slideIndex-1].className += " gallery_active";
+            }
